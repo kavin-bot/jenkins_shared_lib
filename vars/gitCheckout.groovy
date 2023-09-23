8df2cabc94b22342be8a0f7577b7e3f3449248a4
mail <2019wa86475@wilp.bits-pilani.ac.in>
2023-09-23 10:01:00 +0530

    first commit

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,8 @@
+def call(Map stageparams){
+    checkout{
+        [$class: 'GitSCM',
+        branches:[[name: stageparams.branch]],
+        useRemoteConfigs:[[url:stageparams.url]]
+        ]
+    }
+}
