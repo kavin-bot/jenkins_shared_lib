04f90bbcd906c49440fa922424d4f95dbe80ad4f
mail <2019wa86475@wilp.bits-pilani.ac.in>
2023-09-23 10:43:50 +0530

    new commit

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,7 @@
 def call(Map stageparams){
-    checkout{
-        [$class: 'GitSCM',
-        branches:[[name: stageparams.branch]],
-        useRemoteConfigs:[[url:stageparams.url]]
-        ]
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: stageparams.branch]],
+        useRemoteConfigs: [[url:stageparams.url]]
+        ])
     }
-}
